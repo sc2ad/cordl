d13dea9c4216ad34a452eb3ceeb0581b6ea01a9b
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-10-30 00:21:18 -0400

    Rename

--- src/generate/members.rs
+++ src/generate/members.rs
@@ -225,11 +225,14 @@ impl Writable for CppMethodSizeStruct {
             writer,
             "template<>
 struct ::il2cpp_utils::il2cpp_type_check::MetadataGetter<static_cast<void ({}::*)({})>(&{}::{})> {{
-  constexpr static const usize get() {{
+  constexpr static const usize size() {{
+    return 0x{:x};
+  }}
+  constexpr static const usize addrs() {{
     return 0x{:x};
   }}
 }};",
-            self.ty, params_format, self.ty, self.name, self.method_data.estimated_size
+            self.ty, params_format, self.ty, self.name, self.method_data.estimated_size, self.method_data.addrs
         )?;
         Ok(())
     }
