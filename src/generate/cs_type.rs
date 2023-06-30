e403126eef9dd682d2e5852b52f4e431084716db
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2023-06-30 12:07:35 -0400

    Fix global namespace append

--- src/generate/cs_type.rs
+++ src/generate/cs_type.rs
@@ -158,11 +158,7 @@ pub trait CSType: Sized {
         let ns = t.namespace(metadata.metadata);
         let name = t.name(metadata.metadata);
         let full_name = t.full_name(metadata.metadata, true);
-        let cpp_full_name = if ns.is_empty() {
-            format!("GlobalNamespace::{}", config.namespace_cpp(&full_name))
-        } else {
-            config.namespace_cpp(&full_name)
-        };
+        let cpp_full_name = config.namespace_cpp(&full_name);
 
         let mut cpptype = CppType {
             self_tag: tag,
