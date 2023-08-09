7d46b4f97f6849cd34919c606ec1880a1fd37310
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2023-08-09 19:27:28 -0400

    Raise error

--- src/generate/members_serialize.rs
+++ src/generate/members_serialize.rs
@@ -125,9 +125,9 @@ impl Writable for CppFieldDecl {
         let suffixes = suffix_mods.join(" ");
 
         if let Some(value) = &self.value {
-            writeln!(writer, "{prefixes} {ty} {suffixes} {name} = {value};");
+            writeln!(writer, "{prefixes} {ty} {suffixes} {name} = {value};")?;
         } else {
-            writeln!(writer, "{prefixes} {ty} {suffixes} {name};");
+            writeln!(writer, "{prefixes} {ty} {suffixes} {name};")?;
         }
 
         Ok(())
