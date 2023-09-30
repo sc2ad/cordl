dfb795fe9ec25572a343831d8785ff0944ae7d09
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2023-09-30 11:54:11 -0400

    Sanitize reserved names

--- src/generate/config.rs
+++ src/generate/config.rs
@@ -23,8 +23,29 @@ impl GenerationConfig {
     }
 
     pub fn name_cpp(&self, string: &str) -> String {
-        // Coincidentally the same as path_name
-        string.replace(['<', '`', '>', '/', '.', '|', ',', '(', ')', '[', ']'], "_")
+        match string {
+            // https://github.com/sc2ad/Il2Cpp-Modding-Codegen/blob/b3267c7099f0cc1853e57a1118d1bba3884b5f03/Codegen-CLI/Program.cs#L77-L87
+            "alignas" | "alignof" | "and" | "and_eq" | "asm" | "atomic_cancel"
+            | "atomic_commit" | "atomic_noexcept" | "auto" | "bitand" | "bitor" | "bool"
+            | "break" | "case" | "catch" | "char" | "char8_t" | "char16_t" | "char32_t"
+            | "class" | "compl" | "concept" | "const" | "consteval" | "constexpr" | "constinit"
+            | "const_cast" | "continue" | "co_await" | "co_return" | "co_yield" | "decltype"
+            | "default" | "delete" | "do" | "double" | "dynamic_cast" | "else" | "enum"
+            | "explicit" | "export" | "extern" | "false" | "float" | "for" | "friend" | "goto"
+            | "if" | "inline" | "int" | "long" | "mutable" | "namespace" | "new" | "noexcept"
+            | "not" | "not_eq" | "nullptr" | "operator" | "or" | "or_eq" | "private"
+            | "protected" | "public" | "reflexpr" | "register" | "reinterpret_cast"
+            | "requires" | "return" | "short" | "signed" | "sizeof" | "static"
+            | "static_assert" | "static_cast" | "struct" | "switch" | "synchronized"
+            | "template" | "this" | "thread_local" | "throw" | "true" | "try" | "typedef"
+            | "typeid" | "typename" | "union" | "unsigned" | "using" | "virtual" | "void"
+            | "volatile" | "wchar_t" | "while" | "xor" | "xor_eq" | "INT_MAX" | "INT_MIN"
+            | "Assert" | "bzero" | "ID" | "VERSION" | "NULL" | "EOF" | "MOD_ID" => {
+                format!("_cordl_{string}")
+            }
+            // Coincidentally the same as path_name
+            _ => string.replace(['<', '`', '>', '/', '.', '|', ',', '(', ')', '[', ']'], "_"),
+        }
     }
     pub fn generic_nested_name(&self, string: &str) -> String {
         // Coincidentally the same as path_name
