0d988634dac478d713b54f8343b635c1d834c083
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2023-12-10 17:02:39 -0400

    Forward decalre and support ArrayW

--- cordl_internals/concepts.hpp
+++ cordl_internals/concepts.hpp
@@ -4,7 +4,12 @@
 #include <type_traits>
 #include "beatsaber-hook/shared/utils/type-concepts.hpp"
 #include "beatsaber-hook/shared/utils/size-concepts.hpp"
-#include "beatsaber-hook/shared/utils/typedefs-string.hpp"
+
+struct StringW;
+
+template <typename T> struct ArrayW;
+
+struct Il2CppObject;
 
 namespace {
 namespace cordl_internals {
@@ -60,8 +65,8 @@ namespace cordl_internals {
     template<typename T>
     concept cordl_pointer = std::is_pointer_v<T> && il2cpp_utils::value_marker_check_v<std::remove_pointer_t<T>, false>;
 
-    template<typename T>
-    concept cordl_ref_type = cordl_pointer<T> || std::is_same_v<T, ::StringW> || std::is_same_v<T, Il2CppObject*>;
-
+    template <typename T>
+    concept cordl_ref_type =
+        cordl_pointer<T> || std::is_same_v<T, ::StringW> || std::is_same_v<T, ::ArrayW<T>> || std::is_same_v<T, Il2CppObject *>;
 }
 } // end anonymous namespace
